9976a9f5f8bdbe87a0ee230919eea5163a3ee546
SFXRescue <code@sfxrescue.com>
2017-07-11 16:47:54 +1000

    Updated Movement.py
    - Removed arc left and right (unused)
    + Added scanForRobot function to find the IP to host the socket server on (untested)

--- python/actual scripts/movement.py
+++ python/actual scripts/movement.py
@@ -2,6 +2,7 @@ from pyax12.connection import Connection
 import time
 import websockets
 import asyncio
+import subprocess, os
 
 sc = Connection(port="/dev/ttyACM0", baudrate=1000000)
 			
@@ -32,73 +33,75 @@ def bckFwdRun(key, s):								#Motor function for going backwards and forwards
 		runMotorGroup([1,3], bwd)				#run the 1st and 3rd motors backward
 		runMotorGroup([2,4], fwd)				#run the 2nd and 4th motors forward
 		return None								#break out of function
-	#print("forward/backwards??? got a weird key: " + str(key)) #if it got here well... error time
+	print("forward/backwards??? got a weird key: " + str(key)) #if it got here well... error time
 
 def leftRightRun(key, s):								#Function for going left and right on the spot
 	fwd = speedSettingToByteArray(False, False, s)		#var speed to bytes reverse: FALSE; arc: FALSE
 	bwd = speedSettingToByteArray(True, False, s)		#var speed to bytes reverse: TRUE; arc: FALSE
 	if (key == 3):									#--- turn left ---
-		runMotorGroup([1,2], fwd)
-		runMotorGroup([3,4], bwd)#run motors on right side forward and left side backward
+		runMotorGroup([1,3], bytes([0,0]))#run motors on left side back and right side forward
+		runMotor(2, fwd)
+		runMotor(4, bwd)#run motors on right side forward and left side backward
 		return None	
 	elif (key == 4):								#--- turn right ---
-		runMotorGroup([1,2], bwd)
-		runMotorGroup([3,4], fwd)				#run right side back
-	#print("Left and Right??? got a weird key: " + str(key)) #error if neither keys 3 or 4 are given
-
-def arcLeftRight(key, s):							#make robot arc right or left (maybe reverse???????)
-	fwd = speedSettingToByteArray(False, True, s)	#var speed to bytes reverse: FALSE; arc: TRUE
-	bwd = speedSettingToByteArray(True, True, s)	#var speed to bytes reverse: TRUE; arc: TRUE
-	if (key == 5):								#--- arc left ---
+		runMotorGroup([2,4], bytes([0,0]))			#run motors on left side back and right side forward
 		runMotor(1, fwd)
-		runMotor(2, speedSettingToByteArray(False, False,s))
-		runMotor(3, bwd)
-		runMotor(4, speedSettingToByteArray(True, False,s))
-		return None
-	elif (key == 6):
-		runMotor(3, fwd)
-		runMotor(4, speedSettingToByteArray(False, False,s))
-		runMotor(1, bwd)
-		runMotor(2, speedSettingToByteArray(True, False,s))
-		return None
-	#print ("ARC ERROR OMG!!! KEY THAT WASNT 6 or 7 GIVEN!! KEY: " + str(key))
+		runMotor(3, bwd)				#run right side back
+	print("Left and Right??? got a weird key: " + str(key)) #error if neither keys 3 or 4 are given
 
 def getSpeed(buf):#  xy x = KEY y = SPEED
-	#print( "<2:<{}".format(str(buf)[2:]))
+	print( "<2:<{}".format(str(buf)[2:]))
 	return int(str(buf)[2:])
 
 def getKey(buf):#  xy x = KEY y = SPEED
-	#print ("<0<{}".format(str(buf)[0]))
+	print ("<0<{}".format(str(buf)[0]))
 	return int(str(buf)[0])
 
+def scanForRobot():
+	online_ips = []
+	for i in range(1, 6):
+		response = subprocess.Popen(['ping', '-c', '1', '-W', '2', '10.0.2.%s' % i], stdout=subprocess.PIPE)
+		response.wait()
+		if not response.poll():
+			online_ips.append("10.0.2.{}".format(i))	
+	os.system("clear")
+	print (str(len(online_ips)) + " online device(s)!")
+	i = 0
+	for j in online_ips:
+		print(j)
+		if j == "10.0.2.4":
+			return j
+		elif j == "10.0.2.3" and not online_ips[i + 1] == "10.0.2.4":
+			return j
+		i = i + 1
+					
 @asyncio.coroutine
 def run(websocket, path):
-	buf_array = [9, 9]
-	i = 0
 	while True:
 		buf = yield from websocket.recv()
-		print(buf_array)
-		print("Speed: {}".format(getSpeed(buf)))
+		print("< {}".format(buf))
 		if len(buf) > 0:
+			#print(len(str(buf)))
+			print(str(buf))
+			#print("b1234"[2])
 			if (getKey(buf) == 1 or getKey(buf) == 2):
 				#print ("ITS ALIVE!")
 				bckFwdRun(getKey(buf), getSpeed(buf))
 				print("Forward/Backward")
 			elif (getKey(buf) == 3 or getKey(buf) == 4):
+				print ("33333")
 				leftRightRun(getKey(buf), getSpeed(buf))
 				print("Left/Right")
 			elif (getKey(buf) == 5 or getKey(buf) == 6):
 				arcLeftRight(getKey(buf), getSpeed(buf))
-				print("ArcLeft/Right")
-			elif(getSpeed(buf) == 0):
-				buf_array[i] = 0
-				print("zero zero")
+				print("ArcLeft/ArcRight")
+			elif(int(buf) == 0):
 				runMotorGroup([1,2,3,4], bytes([0,0]))
-				
+				print("Stop")
 		else:
 			#runMotorGroup([1,2,3,4], bytes([0,0]))
 			print("not")
 
-start_server = websockets.serve(run, "10.0.2.4", 5555)
+start_server = websockets.serve(run, scanForRobot(), 5555)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
