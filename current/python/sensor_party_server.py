7bbf1b88f9d9fcc6f984c17428c3a021769b86ca
SFXRescue <code@sfxrescue.com>
2017-07-19 12:03:54 +1000

    Added sensor_party_server.py

---
+++ current/python/sensor_party_server.py
@@ -0,0 +1,26 @@
+import websockets, asyncio, serial
+
+ser = serial.Serial("COM7")
+
+@asyncio.coroutine
+def sendSensorData(websocket, path):
+	while True:
+	i = 0
+	messages = []
+	message_str = ""
+		for i in range(7):
+			messages[i] = str(ser.readine()).decode('utf-8')
+		for message in messages:
+			message_str = message_str + " " + message
+		print(message_str)
+		yield from websocket.send(message_str)
+			
+			
+		
+		
+start_server = websockets.serve(sendSensorData, "10.0.2.5", 5555)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+	
+
+
