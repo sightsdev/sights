e200573f152e27e3c597aa0e7b3e88b3b473b619
SART-Jack <SART-Jack@users.noreply.github.com>
2017-07-21 12:30:46 +1000

    Adjusted for RAM input in bytes

--- current/html/assets/performancescript.js
+++ current/html/assets/performancescript.js
@@ -7,9 +7,9 @@ performanceSocket.onmessage = function(event){
 var str = event.data;
 
 var performanceArray = str.split(' '),
-    usedRam = Math.round(performanceArray[0]), totalRam = Math.round(performanceArray[1]), cpu = Math.round(performanceArray[2]), cpuTemp = Math.round(performanceArray[3]), upseconds = Math.round(performanceArray[4]);
+    totalRam = Math.round(performanceArray[0]/1048576), usedRam = Math.round(performanceArray[1]/1048576), cpu = Math.round(performanceArray[2]), cpuTemp = Math.round(performanceArray[3]), upseconds = Math.round(performanceArray[4]);
 	
-if (ram < 1024) {
+if (usedRam < 1024) {
 	document.getElementById("ram").innerHTML = usedRam + " MB";
 }
 else {
@@ -21,7 +21,7 @@ document.getElementById("ramPercentage").className = "c100 big orange p" + Math.
 document.getElementById("cpu").innerHTML = cpu + "%";
 document.getElementById("cpuPercentage").className = "c100 big orange p" + cpu;
 
-document.getElementById("cpuTemp").innerHTML = cpuTemp + "%";
+document.getElementById("cpuTemp").innerHTML = cpuTemp + "&degC";
 document.getElementById("cpuTempPercentage").className = "c100 big orange p" + cpuTemp;
 
 document.getElementById("uptime").innerHTML = new Date(1000 * upseconds).toISOString().substr(11, 8) + "";
