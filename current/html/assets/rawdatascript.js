9cf7164b655e1f6aa4779a2b40b51173fb0b7f09
SART-Jack <SART-Jack@users.noreply.github.com>
2017-10-12 14:28:41 +1100

    Formatting for switchover
    Nobody will be able to maintain my code...

--- current/html/assets/rawdatascript.js
+++ current/html/assets/rawdatascript.js
@@ -5,20 +5,20 @@ var servoSocket = new WebSocket("ws://" + ip + ":5556");
 
 sensorSocket.onmessage = function(event){
 
-var sensorStr = event.data;
-//Need to check the order of the sensors
-var sensorArray = sensorStr.split(' '),
-    sonarLeft = performanceArray[0], sonarFront = performanceArray[1], sonarRight = performanceArray[2], sonarRear = performanceArray[3];
+	var sensorStr = event.data;
+	//Need to check the order of the sensors
+	var sensorArray = sensorStr.split(' '),
+		sonarLeft = performanceArray[0], sonarFront = performanceArray[1], sonarRight = performanceArray[2], sonarRear = performanceArray[3];
 
-//document.getElementById("sonar front element whatever it is")
+	//document.getElementById("sonar front element whatever it is")
 }
 
 servoSocket.onmessage = function(event){
 
-var servoStr = event.data;
+	var servoStr = event.data;
 
-var servoArray = servoStr.split(' '),
-    servoFrontLeft = servoArray[0], servoFrontRight = servoArray[1], servoRearRight = servoArray[2], servoRearLeft = servoArray[3];
+	var servoArray = servoStr.split(' '),
+		servoFrontLeft = servoArray[0], servoFrontRight = servoArray[1], servoRearRight = servoArray[2], servoRearLeft = servoArray[3];
 	
-//document.getElementById("servo front left element whatever it is")
+	//document.getElementById("servo front left element whatever it is")
 }
