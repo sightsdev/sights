f6cd791dc58324175664bd35e36bc4ec1a85cef2
Conr86 <cjkneebone@gmail.com>
2018-10-06 15:11:20 +1000

    Combined performance and sensor scripts

--- sensor_stream.py
+++ sensor_stream.py
@@ -5,6 +5,31 @@ ser = serial.Serial("/dev/ttyACM1", 115200)
 msg = {}
 
 def getData():
+    # Get data from system
+    
+    # Get memory data
+	memory_total = psutil.virtual_memory().total
+	memory_used = psutil.virtual_memory().used
+	# Get CPU usage
+	cpu_percent = psutil.cpu_percent(interval=1)
+	# Get CPU temp
+	highest_temp = 0.0
+	highest_core = ''
+	for i in psutil.sensors_temperatures()['coretemp']:
+		if (i.current > highest_temp):
+			highest_temp = i.current
+			highest_core = i.label
+	# Get uptime
+	with open('/proc/uptime', 'r') as f:
+		uptime = f.readline().split()[0]
+	
+	msg["memory_total"] = str(memory_total)
+	msg["memory_used"] = str(memory_used)
+	msg["cpu_percent"] = str(cpu_percent)
+	msg["highest_temp"] = str(highest_temp)
+	msg["uptime"] = str(uptime)
+	
+    # Get data from Arduino
 	buf = ser.readline().decode("UTF-8")
 	# If string begins with "D:", it's distance
 	if (buf[0] == "D"):
@@ -18,7 +43,7 @@ async def sendSensorData(websocket, path):
 
 def main():
 	print("Starting sensor data server")
-	start_server = websockets.serve(sendSensorData, "10.0.2.4", 5557)
+	start_server = websockets.serve(sendSensorData, "10.0.2.4", 5556)
 	asyncio.get_event_loop().run_until_complete(start_server)
 	asyncio.get_event_loop().run_forever()
 
