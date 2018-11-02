9f862488e108af239c3872582805b336eb75b2db
Connor <cjkneebone@gmail.com>
2018-11-02 16:31:46 +1100

    PID centering works! Code should all be functional now, but normal gamepad controls remain untested.

--- auto_pid.py
+++ auto_pid.py
@@ -6,14 +6,16 @@ from servo_party import ServoParty
 import time
 
 # Servos
-servo_party = ServoParty();
+servo_party = ServoParty()
 # Arduino
 sc_arduino = Serial(port="/dev/ttyACM1", baudrate=115200)
 
 # PID constants
-K_p = 0
+K_p = 2
 K_i = 0
 K_d = 0
+
+speed = 400
 
 class Distance(IntEnum):
     FRONT = 0
@@ -21,57 +23,69 @@ class Distance(IntEnum):
     RIGHT = 2
     BACK = 3
 
+
 def getData():
-	buf = sc_arduino.readline().decode("UTF-8")
-	# If string begins with "D:", it's distance
-	msg = {}
-	if (buf[0] == "D"):
-		# Strip leading "D:" and split by comma
-		msg["dist"] = buf[2:-3].split(",")
-	return msg
+    buf = sc_arduino.readline().decode("UTF-8")
+    # If string begins with "D:", it's distance
+    msg = {}
+    if (buf[0] == "D"):
+        # Strip leading "D:" and split by comma
+        msg["dist"] = buf[2:-3].split(",")
+    return msg
+
 
 def main():
-	integral = 0
-	last_error = 0
-	current_time = time.time()
-	last_time = current_time
+    integral = 0
+    last_error = 0
+    current_time = time.time()
+    last_time = current_time
 
-	while True:
-		msg = getData()
-		if not msg = {}:
-			# Get the four distances
-			front = int(msg["dist"][Distance.FRONT])
-			back = int(msg["dist"][Distance.BACK])
-			left = int(msg["dist"][Distance.LEFT])
-			right = int(msg["dist"][Distance.RIGHT])
+    while True:
+        msg = getData()
+        if not msg == {}:
+            # Get the four distances
+            front = int(msg["dist"][Distance.FRONT])
+            back = int(msg["dist"][Distance.BACK])
+            left = int(msg["dist"][Distance.LEFT])
+            right = int(msg["dist"][Distance.RIGHT])
 
-			# Get current time
-			current_time = time.time()
-			# Change in time since last loop
-			delta_time = current_time - last_time
-			
-			# Our setpoint (SP) is 0, as we want the difference between the two sides to be 0. This is our target value.
-			# Therefore, because our SP is 0, our current measured value is the same as the error which is difference between left and right
-			error = left - right
-			# Work out the change in error since the last loop
-			delta_error = error - last_error
+            if (front < 100):
+                    servo_party.move(0,0)
+            else:
+                    print("l:", left, "r:", right)
 
-			# Our integral is the sum of all previous errors taking into account time
-			integral += error * delta_time
+                    # Get current time
+                    current_time = time.time()
+                    # Change in time since last loop
+                    delta_time = current_time - last_time
 
-			# Our derivative is the difference between this error and the last
-			derivative = 0
-			if (delta_time > 0): # Generally we try to avoid dividing by zero
-				derivative = delta_error / delta_time
+                    # Our setpoint (SP) is 0, as we want the difference between the two sides to be 0. This is our target value.
+                    # Therefore, because our SP is 0, our current measured value is the same as the error which is difference between left and right
+                    error = left - right
+                    # Work out the change in error since the last loop
+                    delta_error = error - last_error
 
-			# Each of our parameters are now multiplied by their respective constants
-			pid = (K_p * error) + (K_i * integral) + (K_d * derivative)
+                    # Our integral is the sum of all previous errors taking into account time
+                    integral += error * delta_time
 
-			# Update last_error to the current error
-			last_error = error
-			# Update last_time
-			last_time = current_time
+                    # Our derivative is the difference between this error and the last
+                    derivative = 0
+                    if (delta_time > 0):  # Generally we try to avoid dividing by zero
+                        derivative = delta_error / delta_time
+
+                    # Each of our parameters are now multiplied by their respective constants
+                    pid = int((K_p * error) + (K_i * integral) +
+                              (K_d * derivative))
+
+                    # Move servos
+                    servo_party.move(speed - pid, speed + pid)
+                    print(pid)
+
+                    # Update last_error to the current error
+                    last_error = error
+                    # Update last_time
+                    last_time = current_time
+
 
 if __name__ == '__main__':
-	main()
- 
+    main()
--- servo_party.py
+++ servo_party.py
@@ -2,46 +2,47 @@
 from pyax12.connection import Connection
 from enum import IntEnum
 
+
 class Servo(IntEnum):
     LEFT_FRONT = 1
     RIGHT_FRONT = 2
     LEFT_BACK = 3
     RIGHT_BACK = 4
-    
+
+
 class ServoParty:
-    sc_dynamixel = Connection(port="/dev/ttyACM0", baudrate=1000000)    
-    speed_factor = 512
-    last_left = 0
-    last_right = 0
-
-    def setup_servo(dynamixel_id):
-        # Set the "wheel mode"
-        sc.set_cw_angle_limit(dynamixel_id, 0, degrees=False)
-        sc.set_ccw_angle_limit(dynamixel_id, 0, degrees=False)
-
-    def move_raw(left, right):
-        # Left side
-        sc_dynamixel.set_speed(Servo.LEFT_FRONT, left)
-        sc_dynamixel.set_speed(Servo.LEFT_BACK, left)
-        # Right side
-        sc_dynamixel.set_speed(Servo.RIGHT_FRONT, right)
-        sc_dynamixel.set_speed(Servo.RIGHT_BACK, right)
-    
-    def move_raw_left(left):
-        # Left side
-        sc_dynamixel.set_speed(Servo.LEFT_FRONT, left)
-        sc_dynamixel.set_speed(Servo.LEFT_BACK, left)
+    def __init__(self):
+        self.sc_dynamixel = Connection(port="/dev/ttyACM0", baudrate=1000000)
+        self.speed_factor = 512
+        self.last_left = 0
+        self.last_right = 0
         
-    def move_raw_right(right):
+    def setup_servo(self, dynamixel_id):
+        # Set the "wheel mode"
+        self.sc_dynamixel.set_cw_angle_limit(dynamixel_id, 0, degrees=False)
+        self.sc_dynamixel.set_ccw_angle_limit(dynamixel_id, 0, degrees=False)
+        
+    def move_raw(self, left, right):
+        # Left side
+        self.sc_dynamixel.set_speed(Servo.LEFT_FRONT, left)
+        self.sc_dynamixel.set_speed(Servo.LEFT_BACK, left)
         # Right side
-        sc_dynamixel.set_speed(Servo.RIGHT_FRONT, right)
-        sc_dynamixel.set_speed(Servo.RIGHT_BACK, right)
+        self.sc_dynamixel.set_speed(Servo.RIGHT_FRONT, right)
+        self.sc_dynamixel.set_speed(Servo.RIGHT_BACK, right)
+    
+    def move_raw_left(self, left):
+        # Left side
+        self.sc_dynamixel.set_speed(Servo.LEFT_FRONT, left)
+        self.sc_dynamixel.set_speed(Servo.LEFT_BACK, left)
+        
+    def move_raw_right(self, right):
+        # Right side
+        self.sc_dynamixel.set_speed(Servo.RIGHT_FRONT, right)
+        self.sc_dynamixel.set_speed(Servo.RIGHT_BACK, right)
 
-    def move(left, right):
-        global last_left
-        global last_right
+    def move(self, left, right):
 
-        # Different motors need to spin in different directions. We account for that here.	
+        # Different motors need to spin in different directions. We account for that here.  
         if (left < 0):
             left *= -1
             left += 1024
@@ -51,9 +52,9 @@ class ServoParty:
             right += 1024
         
         # Only send message if it's different to the last one
-        if (left != last_left and right != last_right):
-            move_servos(left, right)
+        if (left != self.last_left and right != self.last_right):
+            self.move_raw(left, right)
         
         # Store this message for comparison next time
-        last_left = left
-        last_right = right
+        self.last_left = left
+        self.last_right = right
