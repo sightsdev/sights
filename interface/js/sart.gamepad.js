f6224638acb2201e249d6d852300718a10a7c74f
Connor <cjkneebone@gmail.com>
2019-07-10 22:07:42 +1000

    Complete gamepad rewrite to event based system. Also reworked log modal.

--- interface/js/sart.gamepad.js
+++ interface/js/sart.gamepad.js
@@ -3,70 +3,11 @@
 	Licensed under GNU General Public License 3.0
 	
 	Mainly based off https://github.com/luser/gamepadtest created in 2013 by Ted Mielczarek (released as CC0)
-	Current code borrows some constants from Dan Cox (videlais) https://gist.github.com/videlais/8110000
 	
 */
 
-var haveEvents = "GamepadEvent" in window;
-var haveWebkitEvents = "WebKitGamepadEvent" in window;
-var controllers = {};
-var rAF =
-	window.mozRequestAnimationFrame ||
-	window.requestAnimationFrame;
-
-var select_pressed = false;
-
+// WebSocket used for controller data
 var controlSocket;
-	
-var BUTTONS = {
-	FACE_A: 0,
-	FACE_B: 1,
-	FACE_X: 2,
-	FACE_Y: 3,
-	LEFT_BUMPER: 4,
-	RIGHT_BUMPER: 5,
-	LEFT_TRIGGER: 6,
-	RIGHT_TRIGGER: 7,
-	SELECT: 8,
-	START: 9,
-	LEFT_STICK: 10,
-	RIGHT_STICK: 11,
-	PAD_UP: 12,
-	PAD_DOWN: 13,
-	PAD_LEFT: 14,
-	PAD_RIGHT: 15,
-	CENTER_BUTTON: 16
-};
-
-// Sample message format
-var controller_message = {}
-/*
-	button_A : false,
-	button_B : false,
-	button_X : false,
-	button_Y : false,
-	left_bumper : false,
-	right_bumper : false,
-	left_trigger : 0,
-	right_trigger : 0,
-	left_axis_x : 0,
-	left_axis_y : 0,
-	right_axis_x : 0,
-	right_axis_y : 0,
-	button_select : false,
-	button_start : false,
-	left_stick : false,
-	right_stick : false,
-	pad_up: false,
-	pad_down: false,
-	pad_left: false,
-	pad_right: false,
-	center: false,
-	flipped : false
-}
-*/
-
-var last_message = JSON.stringify(controller_message);
 
 //Log to the console the result of the socket connection attempt. Useful for troubleshooting.
 function socketState() {
@@ -83,143 +24,101 @@ function socketState() {
 	}
 }
 
-function connectHandler(e) {
-	addGamepad(e.gamepad);
+// Checks if socket is open, then converts data to JSON and sends it
+function safeSend(data) {
+	//console.log(data);
+	if (controlSocket != undefined && controlSocket.readyState == 1) 
+		controlSocket.send(JSON.stringify(data));
 }
 
-function addGamepad(gamepad) {
-	$("#controller_status").html("<i class='fa fa-fw fa-gamepad'></i>");
-	$("#controller_status").attr("class", "btn btn-success");
-
-	bootoast.toast({
-		"message": "Controller connected",
-		"type": "success",
-		"position": "left-bottom"
-	});
-	
-	controllers[gamepad.index] = gamepad;
-	rAF(updateStatus);
-	console.log("Controller connected");
+function logControl (control, value) {
+	$('#gamepad-log-pre').append("<li>" + new Date().toLocaleTimeString() + " - " + control + " " + value + "</li>" );
 }
 
-function disconnectHandler(e) {
-	removeGamepad(e.gamepad);
-}
-
-function removeGamepad(gamepad) {
-	$("#controller_status").html("<i class='fa fa-fw fa-gamepad'></i> Press a button to connect");
-	$("#controller_status").attr("class", "btn btn-danger");
-	bootoast.toast({
-		"message": "Controller disconnected",
-		"type": "danger",
-		"position": "left-bottom"
-	});	
-	var d = document.getElementById("controller" + gamepad.index);
-	document.body.removeChild(d);
-	delete controllers[gamepad.index];
-}
-
-function getButton(controller, id) {
-	var val = controller.buttons[BUTTONS[id]];
-	var pressed = val == 1.0;
-	if (typeof val == "object") {
-		pressed = val.pressed;
-		val = val.value;
-	}
-	if (pressed) {
-		return true;
-	} else return false;
-}
-
-function getTrigger(controller, id) {
-	var button = controller.buttons[BUTTONS[id]];
-	if (typeof button == "object") {
-		return button.value.toFixed(1);
-	}
-}
-
-function updateStatus() {
-	scanGamepads();
-	
-	for (j in controllers) {
-		var controller = controllers[j];
-			
-		controller_message.button_A = getButton(controller, "FACE_A");
-		controller_message.button_B = getButton(controller, "FACE_B");
-		controller_message.button_X = getButton(controller, "FACE_X");
-		controller_message.button_Y = getButton(controller, "FACE_Y");
-		
-		controller_message.pad_up = getButton(controller, "PAD_UP");
-		controller_message.pad_down = getButton(controller, "PAD_DOWN");
-		controller_message.pad_left = getButton(controller, "PAD_LEFT");
-		controller_message.pad_right = getButton(controller, "PAD_RIGHT");
-		
-		controller_message.left_stick = getButton(controller, "LEFT_STICK");
-		controller_message.right_stick = getButton(controller, "RIGHT_STICK");
-		
-		controller_message.left_bumper = getButton(controller, "LEFT_BUMPER");
-		controller_message.right_bumper = getButton(controller, "RIGHT_BUMPER");
-		
-		controller_message.left_trigger = getTrigger(controller, "LEFT_TRIGGER");
-		controller_message.right_trigger = getTrigger(controller, "RIGHT_TRIGGER");
-
-		// Controller left and right stick axes
-		for (var i = 0; i < controller.axes.length; i++) {
-			if (i == 0) controller_message.left_axis_x = controller.axes[i].toFixed(1);
-			if (i == 1) controller_message.left_axis_y = controller.axes[i].toFixed(1);
-			if (i == 2) controller_message.right_axis_x = controller.axes[i].toFixed(1);
-			if (i == 3) controller_message.right_axis_y = controller.axes[i].toFixed(1);
-		}
-		
-		json_message = JSON.stringify(controller_message);
-		
-		// Compare with last message
-		if (json_message !== last_message) {
-			last_message = json_message;
-			//console.log(json_message);
-			if (controlSocket != undefined && controlSocket.readyState == 1) 
-				controlSocket.send(json_message);
-		}
-	}
-	
-	rAF(updateStatus);
-}
-
-function scanGamepads() {
-	var gamepads = navigator.getGamepads
-		? navigator.getGamepads()
-		: navigator.webkitGetGamepads
-			? navigator.webkitGetGamepads()
-			: [];
-	for (var i = 0; i < gamepads.length; i++) {
-		if (gamepads[i]) {
-			if (!(gamepads[i].index in controllers)) {
-				addGamepad(gamepads[i]);
-			} else {
-				controllers[gamepads[i].index] = gamepads[i];
-			}
-		}
-	}
-}
-
-$(document).ready(function(){
+$(document).ready(function() {
 	console.log("Attempting to connect to the websocket server");
 	
 	try {
 		controlSocket = new WebSocket("ws://" + ip + ":5555");
 		console.log("Attempt result: " + socketState());
 	} catch (err) {
-		console.log(err);
+		console.log("Not connected to controller socket");
 	}
 	
+	// Attach it to the window so it can be inspected at the console.
+	window.gamepad = new Gamepad();
+	var currentGamepad = 0;
+	
+	$('#gamepadSelect').on('change', function (e) {
+		var device = gamepad.gamepads[this.value];
+		$('#gamepad-monitor-pre').html(JSON.stringify(device.state, null, "\t"));
+	});
 
-	if (haveEvents) {
-		window.addEventListener("gamepadconnected", connectHandler);
-		window.addEventListener("gamepaddisconnected", disconnectHandler);
-	} else if (haveWebkitEvents) {
-		window.addEventListener("webkitgamepadconnected", connectHandler);
-		window.addEventListener("webkitgamepaddisconnected", disconnectHandler);
-	} else {
-		setInterval(scanGamepads, 500);
+	gamepad.bind(Gamepad.Event.CONNECTED, function(device) {
+		console.log('Controller connected:', device.id);
+		
+		$("#controller_status").html("<i class='fa fa-fw fa-gamepad'></i>");
+		$("#controller_status").attr("class", "btn btn-success");
+		
+		bootoast.toast({
+			"message": "Controller connected",
+			"type": "success",
+			"position": "left-bottom"
+		});
+		
+		$('#gamepadSelect').append('<option value="' + device.index + '">' + device.id +'</option>');
+		$('#gamepadSelect').trigger('change');
+	});
+
+	gamepad.bind(Gamepad.Event.DISCONNECTED, function(device) {
+		console.log('Controller disconnected', device.id);
+		if (gamepad.count() == 0) {
+			$("#controller_status").html("<i class='fa fa-fw fa-gamepad'></i> Press a button to connect");
+			$("#controller_status").attr("class", "btn btn-danger");
+		}
+		bootoast.toast({
+			"message": "Controller disconnected",
+			"type": "danger",
+			"position": "left-bottom"
+		});
+	});
+
+	gamepad.bind(Gamepad.Event.BUTTON_DOWN, function(e) {
+		var c_event = {
+			control: e.control, 
+			state: "DOWN"
+		};
+		safeSend(c_event);
+		logControl(e.control, "DOWN");
+	});
+
+	gamepad.bind(Gamepad.Event.BUTTON_UP, function(e) {
+		var c_event = {
+			control: e.control, 
+			state: "UP"
+		};
+		safeSend(c_event);
+		logControl(e.control, "UP");
+	});
+
+	gamepad.bind(Gamepad.Event.AXIS_CHANGED, function(e) {
+		if (e.value.toFixed(2) > 0.3 || e.value.toFixed(2) < -0.3) {
+			var c_event = {
+				control: e.axis, 
+				state: e.value.toFixed(2)
+			};
+			safeSend(c_event);
+			logControl(e.axis, "changed to " + e.value);
+		}
+	});
+	
+	gamepad.bind(Gamepad.Event.TICK, function(gamepads) {
+		var gamepad = gamepads[currentGamepad];
+		if (gamepad)
+			$("#gamepad-monitor-pre").html(hljs.highlight("JSON", JSON.stringify(gamepad.state, null, "\t")).value);
+	});
+
+	if (!gamepad.init()) {
+		alert('Your browser does not support gamepads, get the latest Google Chrome or Firefox.');
 	}
 });
--- interface/js/sart.interface.js
+++ interface/js/sart.interface.js
@@ -15,20 +15,23 @@ function portString (port) {
 	return "http://" + ip + ":" + port;
 }
 
-$(document).ready(function(){
+function selectTextInElement (id) {
+	var range = document.createRange();
+	var selection = window.getSelection();
+	range.selectNodeContents(document.getElementById(id));
+	selection.removeAllRanges();
+	selection.addRange(range);
+}
 
-	// Flip cameras
-	//#("#camera_front").attr("style", "transform: scale(-1, -1);");
-	//#("#camera_left").attr("style", "transform: scale(-1, -1);");
-	//#("#camera_right").attr("style", "transform: scale(-1, -1);");
-	//#("#camera_back").attr("style", "transform: scale(-1, -1);");
+$(document).ready(function(){
 	
 	// Allow both a tooltip and a modal window on a button
 	$('[rel="tooltip"]').tooltip({trigger: "hover"});
+	
 	// Enable tooltips
 	$('[data-toggle="tooltip"]').tooltip()
 	
-	// Set to four camera view by default
+	// Set to camera view by default
 	$("#btm_view_sensors").toggle(false);
 	
 	// Allow modals to be draggable
@@ -36,50 +39,41 @@ $(document).ready(function(){
 		handle: ".modal-header"
 	});
 	
+	// Reload page when header pressed
 	$("#nav_title").click(function() {
-		// Reload page
 		location.reload();
 	});
 	
+	// Swap between camera and sensors view
 	$("#sensorToggle").click(function() {
-		// Swap between camera and sensors view
 		$("#btm_view_camera" ).toggle();
 		$("#btm_view_sensors" ).toggle();
 	});
 	
-	$("#log_clear").click(function() {
-	  // Clear log dump box
-	  $("#dump_box").html("");
-	});
-	$("#sensor_dump_button").click(function() {
-	  // Convert sensor data object to JSON (with formatting) and then into syntax highlighted HTML
-	  $("#dump_box").html(hljs.highlight("JSON", JSON.stringify(last_sensor_data, null, "\t")).value);
-	  
-	});
-	$("#controller_dump_button").click(function() {
-	  // Convert controller data object to JSON (with formatting) and then into syntax highlighted HTML
-	  $("#dump_box").html(hljs.highlight("JSON", JSON.stringify(controller_message, null, "\t")).value);
+	// Clear log dump box
+	$("#gamepad-log-clear-button").click(function() {
+	  $("#gamepad-log-pre").html("");
 	});
 	
-	$("#select_log_button").click(function() {
+	// Select the contents of the dump log box
+	$("#gamepad-log-select-button").click(function() {
 		// Select the contents of the dump log box
-		var range = document.createRange();
-		var selection = window.getSelection();
-		range.selectNodeContents(document.getElementById('dump_box'));
-		selection.removeAllRanges();
-		selection.addRange(range);
+		selectTextInElement('gamepad-log-pre');
 	});
 	
+	// If the camera stream doesn't load, default to fallback image
 	$('.streamImage').error(function(){
-		// If the camera stream doesn't load, default to fallback image
 		if (this.src != 'images/no-feed-small.png') this.src = 'images/no-feed-small.png';  
 	});
+	
 	// Set source of camera streams
 	$("#camera_front").attr("src", portString(8081));
 	$("#camera_back").attr("src", portString(8082));
+	
 	// Set source of SSH window
 	$("#ssh_iframe").attr("src", portString(4200));
 	
+	// Generate thermal camera table
 	x = 0;
 	var table = $('<table>');
 	for(i = 0; i < 24; i++){
