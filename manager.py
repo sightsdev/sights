448767249ec8355fec5c629be14ca06c9d0ef3b8
Connor <cjkneebone@gmail.com>
2019-09-09 08:22:44 +1000

    Created new pipe for manager communication, also now using new WebSocketProcess class

--- manager.py
+++ manager.py
@@ -1,50 +1,31 @@
 #!/usr/bin/env python3
-import os
-import websockets
-import multiprocessing
-import asyncio
+from multiprocessing import Pipe
 from control_receiver import ControlReceiver
 from sensor_stream import SensorStream
 
 # Used to handle restarting the scripts
 restartFlag = True
 
-class WebSocketProcess (multiprocessing.Process):
-    def __init__(self, proc, name, instance):
-        multiprocessing.Process.__init__(self)
-        self.proc = proc
-        self.name = name
-        self.instance = instance
-
-    def run(self):
-        print("MANAGER: Starting " + self.name + " process")
-        start_server = websockets.serve(
-            self.instance.run, self.instance.config['network']['ip'], self.instance.port)
-        asyncio.get_event_loop().run_until_complete(start_server)
-        asyncio.get_event_loop().run_forever()
-        print("MANAGER: Exiting " + self.name + " process")
-
-
 def main():
     restartFlag = False
     print("MANAGER: Starting manager process")
-    # Create pipe
-    alpha_pipe, beta_pipe = multiprocessing.Pipe()
-    # Create server and receiver objects
-    server = SensorStream(alpha_pipe)
-    receiver = ControlReceiver(beta_pipe)
-    # Create new process
-    sensor_process = WebSocketProcess(1, "sensor data server", server)
-    control_process = WebSocketProcess(2, "control data receiver", receiver)
+    # Create pipe. sensor_pipe receives, and control_pipe sends
+    sensor_pipe, control_pipe = Pipe(duplex=False)
+    # Create server and receiver processes
+    sensor_process = SensorStream(1, sensor_pipe)
+    control_process = ControlReceiver(2, control_pipe)
+    # Create pipe for control_process to manager communication
+    manager_pipe, super_pipe = Pipe(duplex=False)
+    control_process.manager_pipe = super_pipe
     # Start new processes
     sensor_process.start()
     control_process.start()
     while True:
         # Check if there are messages ready to be received
         # Normally these are received by sensor_stream but we use them to check for manager requests too
-        if alpha_pipe.poll():
+        if manager_pipe.poll():
             # Handle message (received from control_receiver.py)
-            message = alpha_pipe.recv()
+            message = manager_pipe.recv()
             if message[0] == "RESTART_SCRIPTS":
                 # Restart everything
                 print("MANAGER: Terminating processes")
