ee37555a562062fd9a57a91b6940e0d201c3bb68
Connor <cjkneebone@gmail.com>
2019-09-09 11:22:45 +1000

    Updated unfck to use config file

--- stop_servos.py
+++ stop_servos.py
@@ -5,13 +5,16 @@ from pyax12.connection import Connection
 import json
 
 # Load config file
-f = open('robot.json')
-config = json.load(f)
+config = json.load(open('robot.json'))
 
+# Open connection
 sc = Connection(port=config['servo']['port'], baudrate=config['servo']['baudrate'])
 
+# Set all motors to zero
 sc.set_speed(1, 0)
 sc.set_speed(2, 0)
 sc.set_speed(3, 0)
 sc.set_speed(4, 0)
+
+# Close the connection
 sc.close()
--- unfck_motors.py
+++ unfck_motors.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 ''' Clears overheating errors and reenables motors '''
-
 from pyax12.connection import Connection
-sc = Connection("/dev/ttyUSB0", 1000000)
+import json
+
+# Load config file
+config = json.load(open('robot.json'))
+
+# Open connection
+sc = Connection(port=config['servo']['port'], baudrate=config['servo']['baudrate'])
+
+# Clear overheating errors and renable torque or something like that
 sc.write_data(4, 0x18, 0)
 sc.write_data(3, 0x18, 0)
 sc.write_data(4, 0x0E, [0xFF, 0x03])
 sc.write_data(3, 0x0E, [0xFF, 0x03])
 sc.write_data(4, 0x22, [0xFF, 0x03])
 sc.write_data(3, 0x22, [0xFF, 0x03])
+
+# Close the connection
+sc.close()
