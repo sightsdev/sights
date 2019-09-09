bb04307992bf9932a7347ba3504b0e7ba2a8fb9a
Connor <cjkneebone@gmail.com>
2019-09-09 19:58:43 +1000

    Can now exit and restart scripts properly

--- manager.py
+++ manager.py
@@ -3,11 +3,7 @@ from multiprocessing import Pipe
 from control_receiver import ControlReceiver
 from sensor_stream import SensorStream
 
-# Used to handle restarting the scripts
-restartFlag = True
-
 def main():
-    restartFlag = False
     print("MANAGER: Starting manager process")
     # Create pipe. sensor_pipe receives, and control_pipe sends
     sensor_pipe, control_pipe = Pipe(duplex=False)
@@ -20,36 +16,32 @@ def main():
     # Start new processes
     sensor_process.start()
     control_process.start()
-    while True:
-        # Check if there are messages ready to be received
-        # Normally these are received by sensor_stream but we use them to check for manager requests too
-        if manager_pipe.poll():
-            # Handle message (received from control_receiver.py)
-            message = manager_pipe.recv()
-            if message[0] == "RESTART_SCRIPTS":
-                # Restart everything
-                print("MANAGER: Terminating processes")
-                sensor_process.terminate()
-                control_process.terminate()
-                sensor_process.join()
-                control_process.join()
-                restartFlag = True
-                break
-            if message[0] == "KILL_SCRIPTS":
-                # Kill everyone
-                print("MANAGER: Terminating processes")
-                sensor_process.terminate()
-                control_process.terminate()
-                sensor_process.join()
-                control_process.join()
-                break
+	# Receive any messages sent from control_gamepad
+    message = manager_pipe.recv()
+    if message[0] == "RESTART_SCRIPTS":
+        # Restart everything
+        print("MANAGER: Restarting processes")
+        sensor_process.terminate()
+        control_process.terminate()
+        sensor_process.join()
+        control_process.join()
+        return True
+    elif message[0] == "KILL_SCRIPTS":
+        # Kill everyone
+        print("MANAGER: Terminating processes")
+        sensor_process.terminate()
+        control_process.terminate()
+        sensor_process.join()
+        control_process.join()
+        return False
     print("MANAGER: Exiting manager process")
 
 
 if __name__ == '__main__':
     try:
         # If the restart flag is enabled we want to run the main function
-        while (restartFlag):
-            main()
+        while(main()):
+            print("MANAGER: Going to restart")
+        print("MANAGER: All processes ended")
     except KeyboardInterrupt:
         pass
