23f126df8a0a54e920d843902f5f8d4dd8c6f25b
Connor <cjkneebone@gmail.com>
2019-09-16 06:13:01 +1000

    Added more stuff to demo mode

--- interface/js/sart.demo.js
+++ interface/js/sart.demo.js
@@ -35,6 +35,10 @@ function DemoMode() {
 
 	// Uptime
 	$("#uptime").html("4:18:22");
+	// Memory
+	$("#memory").css('color', getColorForPercentage(0.3))
+	$("#memory_used").html(1273);
+	$("#memory_total").html(8192);
 
 	// Generate thermal camera table
 	var table = $("<table>");
@@ -60,21 +64,119 @@ function DemoMode() {
 		}
 	}
 
+	// Update sensor monitor (in log modal)
+	var obj = {
+		distance: [768, 128, 256, 312],
+		thermal_camera: [],
+		co2: 250,
+		tvoc: 75,
+		temp: [22],
+		charge: 0.97,
+		cpu_temp: 45,
+	};
+	$("#sensor-monitor-pre").html(hljs.highlight("JSON", JSON.stringify(obj, null, "\t")).value);
+
+	// Controller status indicator
 	$("#controller-status-connected").show();
 	$("#controller-status-disconnected").hide();
-
+	// Create corresponding toast
 	bootoast.toast({
 		"message": "Controller connected",
 		"icon": "gamepad",
 		"type": "success",
 		"position": "left-bottom"
 	});
-
+	// Active gamepad dropdown menu
 	$('#gamepadSelect').html('<option value="0" id="gamepad">Xbox 360 Controller</option>');
 
+	// Gamepad monitor data
+	obj = {
+		"FACE_1": 0,
+		"FACE_2": 0,
+		"FACE_3": 0,
+		"FACE_4": 0,
+		"LEFT_TOP_SHOULDER": 0,
+		"RIGHT_TOP_SHOULDER": 0,
+		"LEFT_BOTTOM_SHOULDER": 0,
+		"RIGHT_BOTTOM_SHOULDER": 0,
+		"SELECT_BACK": 0,
+		"START_FORWARD": 0,
+		"LEFT_STICK": 0,
+		"RIGHT_STICK": 0,
+		"DPAD_UP": 0,
+		"DPAD_DOWN": 0,
+		"DPAD_LEFT": 0,
+		"DPAD_RIGHT": 0,
+		"HOME": 0,
+		"LEFT_STICK_X": 0.11680781841278076,
+		"LEFT_STICK_Y": 0,
+		"RIGHT_STICK_X": 0,
+		"RIGHT_STICK_Y": 0.03492790460586548
+	}
+	$('#gamepad-monitor-pre').html(hljs.highlight("JSON",JSON.stringify(obj, null, '\t')).value);
+
+	// Config editor box
+	obj = {
+		"network": {
+			"ip": "localhost"
+		},
+		"control": {
+			"default_gamepad_speed": 5,
+			"default_keyboard_speed": 5
+		},
+		"servo": {
+			"port": "/dev/ttyACM0",
+			"baudrate": 1000000
+		},
+		"arduino": {
+			"enabled": false,
+			"port": "/dev/ttyACM1",
+			"baudrate": 115200
+		},
+		"cameras": {
+			"front": {
+				"enabled": true,
+				"port": 8081
+			},
+			"back": {
+				"enabled": true,
+				"port": 8082
+			},
+			"left": {
+				"enabled": false,
+				"port": 8083
+			},
+			"right": {
+				"enabled": false,
+				"port": 8084
+			}
+		},
+		"sensors": {
+			"thermal_camera": {
+				"enabled": true,
+				"width": 32,
+				"height": 24
+			},
+			"temperature": {
+				"enabled": true
+			},
+			"distance_graph": {
+				"enabled": true
+			}
+		},
+		"debug": {
+			"print_messages": false,
+			"use_virtual_servos": true
+		}
+	}
+	var yaml = jsyaml.safeDump(obj, indent = 4);
+	$("#config-editor-pre").html(hljs.highlight("YAML", yaml).value);
+
+	// Set robot connection status indicator
 	$("#robot_status").html("<i class='fa fa-fw fa-link'></i>");
 	$("#robot_status").attr("class", "btn btn-success");
 
+	// Create toast for robot connected
 	bootoast.toast({
 		"message": "Connected to robot",
 		"icon": "link",
@@ -82,8 +184,10 @@ function DemoMode() {
 		"position": "left-bottom"
 	});
 
+	// Hide 'Demo Mode' button and the seperator near it
 	$("#power-options-divider").hide();
 	$("#demo-mode-button").hide();
+
 	// Icing on the cake
 	$("#demo-mode-indicator").show();
 }
