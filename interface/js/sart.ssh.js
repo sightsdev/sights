908bb88834b568fae679aa0fcbc43ff2a9fdec04
Jack Williams <30068301+WilliamsJack@users.noreply.github.com>
2019-09-24 00:51:52 +1000

    Fix terminal focus when load times are longer than expected

--- interface/js/sart.ssh.js
+++ interface/js/sart.ssh.js
@@ -19,16 +19,15 @@ $(document).ready(function() {
 		ssh_count++;
 		let iframe_url = $("#ssh_iframe_1").attr("src"); // We know the correct URL exists in iframe 1 - use it.
 		
-		// Create the tab and terminal
-		$('#ssh-new-tab').before('<li class="nav-item ssh-tab" id="ssh-tab-'+ ssh_count +'"><a class="nav-link text-dark active" href="#ssh-term-' + ssh_count + '" role="tab" data-toggle="tab" aria-selected="true">' + ssh_tab_icon + ssh_count + '</a></li>');
-        $('#ssh-term-content').append($('<div class="tab-pane fade active show" id="ssh-term-' + ssh_count + '"><iframe id="ssh_iframe_'+ ssh_count +'" src="'+ iframe_url +'" width="100%" height="400px"></iframe></div>'));
-		
-		// Go to the new tab
+		// Get ready to go to the new tab
 		$('#ssh-tab-' + ssh_current + ' > a').removeClass("active");
 		$('#ssh-term-' + ssh_current).removeClass("active");
 		$('#ssh-term-' + ssh_current).removeClass("show");
 		ssh_current = ssh_count;
-		focusCurrentSsh();
+		
+		// Create the tab and terminal
+		$('#ssh-new-tab').before('<li class="nav-item ssh-tab" id="ssh-tab-'+ ssh_count +'"><a class="nav-link text-dark active" href="#ssh-term-' + ssh_count + '" role="tab" data-toggle="tab" aria-selected="true">' + ssh_tab_icon + ssh_count + '</a></li>');
+        $('#ssh-term-content').append($('<div class="tab-pane fade active show" id="ssh-term-' + ssh_count + '"><iframe id="ssh_iframe_'+ ssh_count +'" src="'+ iframe_url +'" width="100%" height="400px" onload="focusCurrentSsh()"></iframe></div>'));
     });
 
     var list = document.getElementById("ssh-term-list");
@@ -44,7 +43,6 @@ $(document).ready(function() {
 	// Refresh terminal
 	$("#ssh-refresh-button").click(function () {
 		$("#ssh_iframe_" + ssh_current).attr("src", $("#ssh_iframe_" + ssh_current).attr("src"));
-		focusCurrentSsh();
 	});
 	
 	// Focus the SSH terminal when the user switches tabs
