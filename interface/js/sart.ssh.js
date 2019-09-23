ee626fe120a70b5e1671277374b8e934a8195ca7
Jack Williams <30068301+WilliamsJack@users.noreply.github.com>
2019-09-23 23:06:05 +1000

    Probably should comment that

--- interface/js/sart.ssh.js
+++ interface/js/sart.ssh.js
@@ -17,8 +17,9 @@ var ssh_tab_icon='<i class="fa fa-fw fa-terminal"></i> ';
 $(document).ready(function() {
     $('#ssh-new-tab').click(function() {
 		ssh_count++;
-		let iframe_url = $("#ssh_iframe_1").attr("src");
+		let iframe_url = $("#ssh_iframe_1").attr("src"); // We know the correct URL exists in iframe 1 - use it.
 		
+		// Create the tab and terminal
 		$('#ssh-term-list').append('<li class="nav-item" id="ssh-tab-'+ ssh_count +'"><a class="nav-link text-dark" href="#ssh-term-' + ssh_count + '" role="tab" data-toggle="tab" aria-selected="true">' + ssh_tab_icon + ssh_count + '</a></li>');
         $('#ssh-term-content').append($('<div class="tab-pane fade" id="ssh-term-' + ssh_count + '"><iframe id="ssh_iframe_'+ ssh_count +'" src="'+ iframe_url +'" width="100%" height="400px"></iframe></div>'));
     });
@@ -39,6 +40,7 @@ $(document).ready(function() {
 		focusCurrentSsh();
 	});
 	
+	// Focus the SSH terminal when the user switches tabs
 	$("#ssh-term-list").click(function(){
 		ssh_current = event.target.closest(".nav-item").getAttribute("id").replace(/[^0-9\.]/g, '');
 		focusCurrentSsh();
