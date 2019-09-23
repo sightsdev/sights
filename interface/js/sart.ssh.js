591a402d0d4c75728391fbd58cc133b73178e4d3
Jack Williams <30068301+WilliamsJack@users.noreply.github.com>
2019-09-23 23:01:19 +1000

    Use tabs for switching, allow infinite terminals

--- interface/js/sart.ssh.js
+++ interface/js/sart.ssh.js
@@ -11,8 +11,19 @@ function focusCurrentSsh() {
 		$("#ssh_iframe_" + ssh_current).focus();
 	}, 200);
 }
-	
-$(document).ready(function () {
+
+var ssh_tab_icon='<i class="fa fa-fw fa-terminal"></i> ';
+
+$(document).ready(function() {
+    $('#ssh-new-tab').click(function() {
+		ssh_count++;
+		let iframe_url = $("#ssh_iframe_1").attr("src");
+		
+		$('#ssh-term-list').append('<li class="nav-item" id="ssh-tab-'+ ssh_count +'"><a class="nav-link text-dark" href="#ssh-term-' + ssh_count + '" role="tab" data-toggle="tab" aria-selected="true">' + ssh_tab_icon + ssh_count + '</a></li>');
+        $('#ssh-term-content').append($('<div class="tab-pane fade" id="ssh-term-' + ssh_count + '"><iframe id="ssh_iframe_'+ ssh_count +'" src="'+ iframe_url +'" width="100%" height="400px"></iframe></div>'));
+    });
+
+    var list = document.getElementById("ssh-term-list");
 	
 	$("#sshModal").on('shown.bs.modal', function () {
 		focusCurrentSsh(); // Focus the current SSH iframe on modal open
@@ -21,53 +32,15 @@ $(document).ready(function () {
 			$("#ssh_iframe_1").attr("src", portString(4200));
 		}
 	});
-
-	// Create a new SSH terminal
-	$("#ssh-new-button").click(function () {
-		//Hide the current terminal and deactivate current button
-		$("#ssh_iframe_" + ssh_current).hide();
-		$("#ssh-tab-" + ssh_current).removeClass("active");
-		
-		//Update counts
-		ssh_count ++;
-		ssh_current = ssh_count;
-		
-		//Create a new iframe
-		$(".ssh-container").append($("#ssh_iframe_1").clone().attr("id","ssh_iframe_" + ssh_count));
-		$("#ssh_iframe_" + ssh_current).show();
-		
-		//Create a button for the new terminal
-		let new_tab = $("#ssh-tab-1").clone(); 				//Clone existing button
-		new_tab.html(function(n,content){ 				//Set last html character to new number
-			return content.substr(0,content.length - 1) + ssh_current;
-		});
-		new_tab.attr("id", "ssh-tab-" + ssh_current); 		//Set id
-		new_tab.addClass("active");
-		$(".ssh-tab-container").append(new_tab);
-		
-		//Focus back on the new SSH terminal
-		focusCurrentSsh();
-		
-		//Limit number of tabs
-		if (ssh_count >= Math.floor($(".ssh-tab-container").width()/67) || ssh_count >= 9) {
-			$("#ssh-new-button").attr("disabled",true);
-			$("#ssh-new-button").parent.attr("rel", "");
-		}
-	});
-	
-	// Switch to another terminal
-	$(".ssh-tab-container").click(function(){
-		$("#ssh_iframe_" + ssh_current).hide();
-		$("#ssh-tab-" + ssh_current).removeClass("active");
-		ssh_current = event.target.closest(".ssh-tab").getAttribute("id").slice(-1);
-		$("#ssh_iframe_" + ssh_current).show();
-		$("#ssh-tab-" + ssh_current).addClass("active");
-		focusCurrentSsh();
-	});
 	
 	// Refresh terminal
 	$("#ssh-refresh-button").click(function () {
 		$("#ssh_iframe_" + ssh_current).attr("src", $("#ssh_iframe_" + ssh_current).attr("src"));
+		focusCurrentSsh();
+	});
+	
+	$("#ssh-term-list").click(function(){
+		ssh_current = event.target.closest(".nav-item").getAttribute("id").replace(/[^0-9\.]/g, '');
 		focusCurrentSsh();
 	});
 });
