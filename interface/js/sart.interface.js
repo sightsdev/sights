44ff17e09adab56b1351d80320a86f39cb34fc06
Jack Williams <30068301+WilliamsJack@users.noreply.github.com>
2019-09-27 02:40:02 +1000

    Add tacky camera flash animation
    Hey, just give me this one.

--- interface/js/sart.interface.js
+++ interface/js/sart.interface.js
@@ -113,7 +113,9 @@ $(document).ready(function () {
 		//Get camera ID from port. Safe for up to 9 cameras as long as properly configured in motion.
 		let url = new URL($(this).closest('.cameraWrapper').find('.streamImage').attr("src"));
 		let cameraId = url.port.charAt(url.port.length-1);
+		let cameraWrapper = $(this).closest('.cameraWrapper')
 		$.get('http://' + ip + ':8080/' + cameraId + '/action/snapshot', function(){
+			cameraWrapper.fadeOut(150).fadeIn(150);
 			let link = document.createElement('a');
 			link.href = 'images/downloads/screenshot.jpg';
 			link.download = 'screenshot.jpg';
