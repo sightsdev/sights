4d7f4d31f7cf01c6d989076e78d0f7121602fc2e
Connor <cjkneebone@gmail.com>
2019-09-30 22:21:38 +1000

    Added rounding to logs and bottom of SSH iframe

--- interface/js/sart.ssh.js
+++ interface/js/sart.ssh.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 		
 		// Create the tab and terminal
 		$('#ssh-new-tab').before('<li class="nav-item ssh-tab" id="ssh-tab-'+ ssh_count +'"><a class="nav-link text-dark active" href="#ssh-term-' + ssh_count + '" role="tab" data-toggle="tab" aria-selected="true">' + ssh_tab_icon + ssh_count + '</a></li>');
-        $('#ssh-term-content').append($('<div class="tab-pane fade active show" id="ssh-term-' + ssh_count + '"><iframe id="ssh_iframe_'+ ssh_count +'" src="'+ iframe_url +'" width="100%" height="400px" onload="focusCurrentSsh()"></iframe></div>'));
+        $('#ssh-term-content').append($('<div class="tab-pane fade active show" id="ssh-term-' + ssh_count + '"><iframe id="ssh_iframe_'+ ssh_count +'" src="'+ iframe_url +'" width="100%" height="400px" onload="focusCurrentSsh()" class="rounded-bottom"></iframe></div>'));
     });
 	
 	$("#sshModal").on('shown.bs.modal', function () {
