bf85a728dc33148f678991c55a4f6ca41259e9a6
Conr86 <cjkneebone@gmail.com>
2019-10-13 19:38:31 +1100

    unfck: add config file as argument

--- utils/unfck_motors.py
+++ utils/unfck_motors.py
@@ -4,8 +4,19 @@
 from pyax12.connection import Connection
 import json
 
+# Setup argument parser for config file loading
+parser = ArgumentParser()
+# Create argument for config file
+parser.add_argument("-c", "--config", 
+                    dest="config_file", 
+                    help="load specified configuration file", 
+                    metavar="<file>", 
+                    default=path+"/opt/sart/SARTRobot/configs/default.json")
+# Actually parse the arguments
+args = parser.parse_args()
+
 # Load config file
-config = json.load(open('robot.json'))
+config = json.load(open(args.config_file))
 
 # Open connection
 sc = Connection(port=config['servo']['port'], baudrate=config['servo']['baudrate'])
