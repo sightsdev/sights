0711b99951982884cb8f4e403098bdeefa63ceab
Connor <cjkneebone@gmail.com>
2019-11-07 21:24:12 +1100

    Removed server.py

--- interface/server.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 80
-
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
