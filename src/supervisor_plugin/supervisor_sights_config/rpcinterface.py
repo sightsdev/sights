c7d2c0cc76fc981a9b762a9044dd60411c9a6883
WilliamsJack <30068301+WilliamsJack@users.noreply.github.com>
2019-12-05 23:21:45 +1100

    Implement method to request config via xml-rpc

--- src/supervisor_plugin/supervisor_sights_config/rpcinterface.py
+++ src/supervisor_plugin/supervisor_sights_config/rpcinterface.py
@@ -8,6 +8,7 @@ API_VERSION = '0.2'
 ACTIVE_CONFIG_FILE = '/opt/sights/SIGHTSRobot/configs/ACTIVE_CONFIG'
 CONFIG_DIR = '/opt/sights/SIGHTSRobot/configs'
 CONFIG_EXT = '.json'
+MINIMAL_CONFIG = "/opt/sights/SIGHTSRobot/src/configs/sights/minimal.json"
 
 class SIGHTSConfigNamespaceRPCInterface:
     """ An extension for Supervisor that implements a basic 
@@ -39,8 +40,8 @@ class SIGHTSConfigNamespaceRPCInterface:
         return True
 
     def getActiveConfig(self):
-        """ Gets contents of file
-        @return string      Contents of file
+        """ Gets the file name of the active config
+        @return string      Contents of ACTIVE_CONFIG_FILE
         """
         try:
             with open(ACTIVE_CONFIG_FILE, 'r') as f:
@@ -57,6 +58,30 @@ class SIGHTSConfigNamespaceRPCInterface:
         if path.isfile(config):
             remove(config)
         return True
+
+    def requestConfig(self):
+        """ Gets the current config
+        @return string       Contents of config file
+        """
+
+        # What do you suppose is the best way to get the active config file name here?
+        # Duplicates getActiveConfig().
+        try:
+            with open(ACTIVE_CONFIG_FILE, 'r') as f:
+                file_path = CONFIG_DIR + f.read()
+                if not path.isfile(file_path):
+                    raise FileNotFoundError()
+        except FileNotFoundError:
+            # If ACTIVE_CONFIG_FILE could not be read, or active config doesn't exist
+            file_path = MINIMAL_CONFIG
+
+        # Now read the active config
+        try:
+            with open(file_path, 'r') as f:
+                read_data = f.read()
+        except FileNotFoundError:
+            read_data = ""
+        return read_data
 
 def make_sights_config_rpcinterface(supervisord, **config):
     return SIGHTSConfigNamespaceRPCInterface(supervisord)
