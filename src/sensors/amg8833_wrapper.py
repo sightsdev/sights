67b468048445ebb8bfed2fc19d0101bf6d8da859
Connor <cjkneebone@gmail.com>
2019-12-17 19:53:56 +1100

    Added support for different AMG833 addresses

--- src/sensors/amg8833_wrapper.py
+++ src/sensors/amg8833_wrapper.py
@@ -7,8 +7,10 @@ class AMG8833Wrapper(SensorWrapper):
 
     def __init__(self, config):
         SensorWrapper.__init__(self, config)
+        # Additional config option for i2c address, default to 0x69
+        self.address = int(config.get('address', "0x69"), 16)
         # Create sensor object
-        self.sensor = AMG88xx()
+        self.sensor = AMG88xx(address=self.address)
 
     def get_data(self):
         return self.sensor.readPixels()
