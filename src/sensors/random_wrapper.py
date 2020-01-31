81bf636cc1770f240321b5d9ef319f022444627c
WilliamsJack <30068301+WilliamsJack@users.noreply.github.com>
2020-01-31 23:00:20 +1100

    Limit no longer needs to be reported by this sensor

--- src/sensors/random_wrapper.py
+++ src/sensors/random_wrapper.py
@@ -9,11 +9,6 @@ class RandomWrapper(SensorWrapper):
         SensorWrapper.__init__(self, config)
         self.min = int(config.get('min', 25))
         self.max = int(config.get('max', 30))
-        self.limit = int(config.get('limit', 50))
-
-    def get_initial(self):
-        # On circle graphs, set the maximum value (limit)
-        return {"limit": self.limit}
 
     def get_data(self):
         return random.randint(self.min, self.max)
