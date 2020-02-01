5729c9ee18ea1ece3d0e6e30fc7557416b5ee07c
WilliamsJack <30068301+WilliamsJack@users.noreply.github.com>
2020-02-01 22:52:26 +1100

    Make it harder to click the SSH tab close button accidentally

--- interface/js/sights.ssh.js
+++ interface/js/sights.ssh.js
@@ -31,12 +31,10 @@ function closeSshTab(tab) {
 	}
 }
 
-var ssh_tab_icon='<i class="fa fa-fw fa-terminal"></i> ';
 
 $(document).on("ready", function() {
     $('#ssh_new_tab').on("click", function() {
 		ssh_count++;
-		let iframe_url = $("#ssh_iframe_1").attr("src"); // We know the correct URL exists in iframe 1 - use it.
 		
 		// Get ready to go to the new tab. Make sure no other tab is shown.
 		$(".ssh-tab").each(function () {
@@ -50,7 +48,7 @@ $(document).on("ready", function() {
 		ssh_current = ssh_count;
 		
 		// Create the tab and terminal
-		$('#ssh_new_tab').before('<li class="nav-item ssh-tab" id="ssh_tab_'+ ssh_current +'"><a class="nav-link text-dark active" ssh_tab="' + ssh_current + '" href="#ssh_term_' + ssh_current + '" role="tab" data-toggle="tab" aria-selected="true" onclick="setCurrentSsh($(this));">' + ssh_tab_icon + ssh_current + '<span ssh_tab="' + ssh_current + '" id="ssh_tab_close_' + ssh_current + '" style="padding-left: 1em" onclick="closeSshTab($(this));">&times</span></a></li>');
+		$('#ssh_new_tab').before('<li class="nav-item ssh-tab" id="ssh_tab_'+ ssh_current +'"><a class="nav-link text-dark active" ssh_tab="' + ssh_current + '" href="#ssh_term_' + ssh_current + '" role="tab" data-toggle="tab" aria-selected="true" onclick="setCurrentSsh($(this));"><i class="fa fa-fw fa-terminal"></i>' + ssh_current + '<span style="padding-right: 1em"></span><span ssh_tab="' + ssh_current + '" id="ssh_tab_close_' + ssh_current + '" onclick="closeSshTab($(this));">&times</span></a></li>');
         $('#ssh_term_content').append($('<div class="tab-pane fade active show ssh-term" id="ssh_term_' + ssh_current + '"><iframe id="ssh_iframe_'+ ssh_current +'" src="" width="100%" height="400px" class="rounded-bottom ssh-iframe"></iframe></div>'));
 		$("#ssh_iframe_" + ssh_current).attr("src", portString(4200));
     });
