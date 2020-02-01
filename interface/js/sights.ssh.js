51a22337e5a2fdb280ffcad712bb4fab956ca274
WilliamsJack <30068301+WilliamsJack@users.noreply.github.com>
2020-02-01 22:42:53 +1100

    You can close SSH tabs just like I closed #41.

--- interface/js/sights.ssh.js
+++ interface/js/sights.ssh.js
@@ -5,11 +5,30 @@
 
 var ssh_count = 1;
 var ssh_current = 1;
-	
-function focusCurrentSsh() {
-	setTimeout(function() {
-		$("#ssh_iframe_" + ssh_current).focus();
-	}, 200);
+
+function setCurrentSsh(tab) {
+	let tab_id = tab.attr("ssh_tab");
+	ssh_current = tab_id;
+}
+
+function closeSshTab(tab) {
+	let tab_id = tab.attr("ssh_tab");
+	$("#ssh_tab_" + tab_id).remove();
+	$("#ssh_term_" + tab_id).remove();
+	let tabActive = false;
+	// If another tab is currently selected, set ssh_current
+	$(".ssh-tab").each(function () {
+		if ($(this).children('a').first().hasClass("active")) {
+			setCurrentSsh($(this).children('a').first());
+			tabActive = true;
+		}
+	});
+	// Set a new current tab if there wasn't another already selected
+	if (!tabActive) {
+		console.log("Finding another tab to show");
+		$(".ssh-tab").first().find("a").addClass("active");
+		$(".ssh-term").first().addClass("active show");
+	}
 }
 
 var ssh_tab_icon='<i class="fa fa-fw fa-terminal"></i> ';
@@ -19,19 +38,24 @@ $(document).on("ready", function() {
 		ssh_count++;
 		let iframe_url = $("#ssh_iframe_1").attr("src"); // We know the correct URL exists in iframe 1 - use it.
 		
-		// Get ready to go to the new tab
-		$('#ssh_tab_' + ssh_current + ' > a').removeClass("active");
-		$('#ssh_term_' + ssh_current).removeClass("active");
-		$('#ssh_term_' + ssh_current).removeClass("show");
+		// Get ready to go to the new tab. Make sure no other tab is shown.
+		$(".ssh-tab").each(function () {
+			$(this).children('a').first().removeClass("active");
+		});
+		$(".ssh-term").each(function () {
+			$(this).removeClass("active");
+			$(this).removeClass("show");
+		});
+
 		ssh_current = ssh_count;
 		
 		// Create the tab and terminal
-		$('#ssh_new_tab').before('<li class="nav-item ssh-tab" id="ssh_tab_'+ ssh_count +'"><a class="nav-link text-dark active" href="#ssh_term_' + ssh_count + '" role="tab" data-toggle="tab" aria-selected="true">' + ssh_tab_icon + ssh_count + '</a></li>');
-        $('#ssh_term_content').append($('<div class="tab-pane fade active show" id="ssh_term_' + ssh_count + '"><iframe id="ssh_iframe_'+ ssh_count +'" src="'+ iframe_url +'" width="100%" height="400px" onload="focusCurrentSsh()" class="rounded-bottom ssh-iframe"></iframe></div>'));
+		$('#ssh_new_tab').before('<li class="nav-item ssh-tab" id="ssh_tab_'+ ssh_current +'"><a class="nav-link text-dark active" ssh_tab="' + ssh_current + '" href="#ssh_term_' + ssh_current + '" role="tab" data-toggle="tab" aria-selected="true" onclick="setCurrentSsh($(this));">' + ssh_tab_icon + ssh_current + '<span ssh_tab="' + ssh_current + '" id="ssh_tab_close_' + ssh_current + '" style="padding-left: 1em" onclick="closeSshTab($(this));">&times</span></a></li>');
+        $('#ssh_term_content').append($('<div class="tab-pane fade active show ssh-term" id="ssh_term_' + ssh_current + '"><iframe id="ssh_iframe_'+ ssh_current +'" src="" width="100%" height="400px" class="rounded-bottom ssh-iframe"></iframe></div>'));
+		$("#ssh_iframe_" + ssh_current).attr("src", portString(4200));
     });
 	
 	$("#ssh_modal").on('shown.bs.modal', function () {
-		focusCurrentSsh(); // Focus the current SSH iframe on modal open
 		// Set the src of the modal on first load only
 		if ($("#ssh_iframe_1").attr("src") == "") {	// Prevent refresh every time the modal is loaded
 			$("#ssh_iframe_1").attr("src", portString(4200));
@@ -41,14 +65,5 @@ $(document).on("ready", function() {
 	// Refresh terminal
 	$("#ssh_refresh_button").on("click", function () {
 		$("#ssh_iframe_" + ssh_current).attr("src", $("#ssh_iframe_" + ssh_current).attr("src"));
-	});
-	
-	// Focus the SSH terminal when the user switches tabs
-	$("#ssh_term_list").on("click", function(){
-		try {
-			ssh_current = event.target.closest(".ssh-tab").getAttribute("id").replace(/[^0-9\.]/g, '');
-			focusCurrentSsh();
-		} catch (e) {
-		}
 	});
 });
