4d4186ee5a5bfabb725aade81d761c0e9d6dd962
Connor <cjkneebone@gmail.com>
2020-02-20 20:37:41 +1100

    Fixed loading if motor wrapper not found

--- src/motor_handler.py
+++ src/motor_handler.py
@@ -16,21 +16,26 @@ class Motors:
         self.pm = PluginManager(MotorWrapper, os.getcwd() + "/src/motors")
         # Load values from configuration file
         self.type = config['motors']['type'].lower()
+        # Log loaded type
+        self.logger.info(f"Opening motor connection of type '{self.type}'")
         try:
             # Create motor connection (from a list loaded by the plugin manager) using class specified in the config
             self.connection = self.pm.wrappers[self.type](config['motors'])
+        except KeyError:
+            self.logger.error("Could not determine motor connection type")
+            self.logger.warning("Falling back to virtual connection")
+            self.connection = VirtualConnection(config['motors'])
+            self.type = 'virtual'
         except serial.serialutil.SerialException:
-            self.logger.warning("Could not determine motor connection type")
-            self.logger.info("Falling back to virtual connection")
-            self.connection = VirtualConnection()
+            self.logger.error(f"Could not open motor connection of type '{self.type}'")
+            self.logger.warning("Falling back to virtual connection")
+            self.connection = VirtualConnection(config['motors'])
             self.type = 'virtual'
         # Load speed defaults
         self.gamepad_speed = config['control']['default_gamepad_speed'] * 128 - 1
         self.keyboard_speed = config['control']['default_keyboard_speed'] * 128 - 1
         self.last_left = 0
         self.last_right = 0
-        # Log loaded type
-        self.logger.info(f"Opening motor connection of type '{self.type}'")
         # Ensure Connection class has access to logging capabilities
         self.connection.logger = self.logger
         
