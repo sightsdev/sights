3d6c3773191a0b019928603d45443bb7cb72c4f9
Connor <cjkneebone@gmail.com>
2020-03-14 23:22:44 +1100

    Minor spacing fix to SSH tabs

--- interface/js/sights.ssh.js
+++ interface/js/sights.ssh.js
@@ -48,7 +48,7 @@ $(document).on("ready", function() {
 		ssh_current = ssh_count;
 		
 		// Create the tab and terminal
-		$('#ssh_new_tab').before('<li class="nav-item ssh-tab" id="ssh_tab_'+ ssh_current +'"><a class="nav-link text-dark active" ssh_tab="' + ssh_current + '" href="#ssh_term_' + ssh_current + '" role="tab" data-toggle="tab" aria-selected="true" onclick="setCurrentSsh($(this));"><i class="fa fa-fw fa-terminal"></i>' + ssh_current + '<span style="padding-right: 1em"></span><span ssh_tab="' + ssh_current + '" id="ssh_tab_close_' + ssh_current + '" onclick="closeSshTab($(this));">&times</span></a></li>');
+		$('#ssh_new_tab').before('<li class="nav-item ssh-tab" id="ssh_tab_'+ ssh_current +'"><a class="nav-link text-dark active" ssh_tab="' + ssh_current + '" href="#ssh_term_' + ssh_current + '" role="tab" data-toggle="tab" aria-selected="true" onclick="setCurrentSsh($(this));"><i class="fa fa-fw fa-terminal"></i> ' + ssh_current + '<span style="padding-right: 1em"></span><span ssh_tab="' + ssh_current + '" id="ssh_tab_close_' + ssh_current + '" onclick="closeSshTab($(this));">&times</span></a></li>');
         $('#ssh_term_content').append($('<div class="tab-pane fade active show ssh-term" id="ssh_term_' + ssh_current + '"><iframe id="ssh_iframe_'+ ssh_current +'" src="" width="100%" height="400px" class="rounded-bottom ssh-iframe"></iframe></div>'));
 		$("#ssh_iframe_" + ssh_current).attr("src", ssh_address);
     });
