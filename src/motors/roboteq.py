d0978bc1c8353690d9d8d49205a0c3a1756e914b
Connor Kneebone <Connor.Kneebone@fusion-centre.com>
2021-06-21 11:23:03 +1000

    Fixed Roboteq wrapper, probably

--- src/motors/roboteq.py
+++ src/motors/roboteq.py
@@ -1,6 +1,7 @@
 from motor_wrapper import MotorWrapper
-import serial
-import logging
+from PyRoboteq import RoboteqHandler
+from PyRoboteq import roboteq_commands as cmds
+
 
 class RoboteqConnection(MotorWrapper):
     # What type of motor this wrapper handles
@@ -8,11 +9,9 @@ class RoboteqConnection(MotorWrapper):
 
     def __init__(self, config):
         MotorWrapper.__init__(self, config)
-        from PyRoboteq import RoboteqHandler
-        from PyRoboteq import roboteq_commands as cmds
 
         self.port = config.get('port')
-        self.controller = RoboteqHandler(debug_mode = True, exit_on_interrupt = False)
+        self.controller = RoboteqHandler(debug_mode=True, exit_on_interrupt=False)
         self.is_connected = self.controller.connect(self.port)
         self.channels = config.get('channels')
         try:
@@ -21,17 +20,20 @@ class RoboteqConnection(MotorWrapper):
         except AttributeError:
             self.channels['left'] = 1
             self.channels['right'] = 0
+        self.last_left = 0
+        self.last_right = 0
 
     def move_raw(self, left=None, right=None):
         # Left side
         if left is not None:
-            self.controller.send_command(SET_SPEED, self.channels.get('left'), abs(round(1000 / 1024 * left)))
+            last_left = abs(round(1000 / 1024 * left))
         # Right side
         if right is not None:
-            self.controller.send_command(SET_SPEED, self.channels.get('right'), abs(round(1000 / 1024 * right)))
+            last_right = abs(round(1000 / 1024 * right))
+        self.controller.send_command(cmds.DUAL_DRIVE, last_left, last_right)
 
     def stop(self):
         self.controller.send_command(cmds.DUAL_DRIVE, 0, 0)
 
     def close(self):
-        print("Not implemented")
+        self.controller = None
