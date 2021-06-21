28760d9a6132d054a16c07bf72e73649e90e842d
root <root@DESKTOP-RS3J0VP.localdomain>
2021-06-21 21:46:30 +1000

    Fixed RoboClaw driver

--- src/motors/RoboClaw/pyroboclaw/roboclaw.py
+++ src/motors/RoboClaw/pyroboclaw/roboclaw.py
@@ -5,21 +5,71 @@ from threading import Lock
 import serial
 import struct
 #from pycrc.CRCCCITT import CRCCCITT
+from ctypes import c_ushort
 
 from pyroboclaw.roboclaw_cmd import Cmd
 
 logger = logging.getLogger('pyroboclaw')
 logger.setLevel(logging.WARNING)
 
-def CRCCITT(crc, data):
-    msb = crc >> 8
-    lsb = crc & 255
-    for c in data:
-        x = ord(c) ^ msb
-        x ^= (x >> 4)
-        msb = (lsb ^ (x >> 3) ^ (x << 4)) & 255
-        lsb = (x ^ (x << 5)) & 255
-    return (msb << 8) + lsb
+
+class CRCCCITT(object):
+    crc_ccitt_tab = []
+
+    # The CRC's are computed using polynomials.
+    # Here is the most used coefficient for CRC CCITT
+    crc_ccitt_constant = 0x1021
+
+    def __init__(self, version='XModem'):
+        try:
+            dict_versions = {'XModem': 0x0000, 'FFFF': 0xffff, '1D0F': 0x1d0f}
+            if version not in dict_versions.keys():
+                raise Exception("Your version parameter should be one of \
+                    the {} options".format("|".join(dict_versions.keys())))
+
+            self.starting_value = dict_versions[version]
+
+            # initialize the precalculated tables
+            if not len(self.crc_ccitt_tab):
+                self.init_crc_ccitt()
+        except Exception as e:
+            print("EXCEPTION(calculate): {}".format(e))
+
+    def calculate(self, input_data=None):
+        try:
+            is_string = isinstance(input_data, str)
+            is_bytes = isinstance(input_data, bytes)
+
+            if not is_string and not is_bytes:
+                raise Exception("Please provide a string or a byte sequence \
+                    as argument for calculation.")
+
+            crcValue = self.starting_value
+
+            for c in input_data:
+                d = ord(c) if is_string else c
+                tmp = (c_ushort(crcValue >> 8).value) ^ d
+                crcValue = (c_ushort(crcValue << 8).value) ^ int(
+                    self.crc_ccitt_tab[tmp], 0)
+
+            return crcValue
+        except Exception as e:
+            print("EXCEPTION(calculate): {}".format(e))
+
+    def init_crc_ccitt(self):
+        '''The algorithm uses tables with precalculated values'''
+        for i in range(0, 256):
+            crc = 0
+            c = i << 8
+
+            for j in range(0, 8):
+                if ((crc ^ c) & 0x8000):
+                    crc = c_ushort(crc << 1).value ^ self.crc_ccitt_constant
+                else:
+                    crc = c_ushort(crc << 1).value
+
+                c = c_ushort(c << 1).value  # equivalent of c = c << 1
+            self.crc_ccitt_tab.append(hex(crc))
 
 class RoboClaw:
     def __init__(self, port, address, auto_recover=False, **kwargs):
@@ -45,7 +95,7 @@ class RoboClaw:
             with self.serial_lock:
                 self.port.write(cmd_bytes)
                 return_bytes = self.port.read(struct.calcsize(fmt) + 2)
-            crc_actual = CRCCCITT(cmd_bytes + return_bytes[:-2])
+            crc_actual = CRCCCITT().calculate(cmd_bytes + return_bytes[:-2])
             crc_expect = struct.unpack('>H', return_bytes[-2:])[0]
             if crc_actual != crc_expect:
                 logger.error('read crc failed')
@@ -61,7 +111,7 @@ class RoboClaw:
     def _write(self, cmd, fmt, *data):
         cmd_bytes = struct.pack('>BB', self.address, cmd)
         data_bytes = struct.pack(fmt, *data)
-        write_crc = CRCCCITT(cmd_bytes + data_bytes)
+        write_crc = CRCCCITT().calculate(cmd_bytes + data_bytes)
         crc_bytes = struct.pack('>H', write_crc)
         try:
             with self.serial_lock:
